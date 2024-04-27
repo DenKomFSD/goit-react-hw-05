073401fdc89d105428bb3a15c5174d279bb5c911
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-04-27 22:59:51 +0300

    first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Homepage from "./pages/HomePage/HomePage";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
+
+const getNavLinkActive = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
+
+export default function Navigation() {
+  return (
+    <nav>
+      <NavLink to="/" className={getNavLinkActive}>
+        Homepage{" "}
+      </NavLink>
+      <NavLink to="/movies" className={getNavLinkActive}>
+        Movies{" "}
+      </NavLink>
+    </nav>
+  );
+}
